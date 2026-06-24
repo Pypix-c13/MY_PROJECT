d6b6af47916f170b7c608cf2c58ccbf04da60696
codingmc8 <codingmc8@gmail.com>
2026-06-24 14:11:00 +0700

    Update

--- apm.py
+++ apm.py
@@ -1,11 +1,9 @@
 from pathlib import Path
 
-class AutoPackageManager:
-    @staticmethod
-    def scanner():
-        current_directory = Path('.')
-        for items in current_directory.iterdir():
-            if items.suffix == ".py":
-                print(items.name)
+def scanner():
+    current_directory = Path('.')
+    for items in current_directory.iterdir():
+        if items.suffix == ".py":
+            print(items.name)
 
-AutoPackageManager.scanner()
+scanner()
